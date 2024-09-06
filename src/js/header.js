6fb20052ff5846b816f3d4f5931fd1e509f61ae9
Michal Mirek <michal.mirek98@gmail.com>
2024-09-06 22:06:17 +0200

    [BC-20] Stworzenie Navbar #15

--- src/js/header.js
+++ src/js/header.js
@@ -3,7 +3,7 @@ toggleStickyToNavbar();
 function toggleStickyToNavbar() {
   const navbar = document.querySelector('nav');
   window.addEventListener('scroll', () => {
-    if (document.documentElement.scrollTop > 60) {
+    if (document.documentElement.scrollTop > 256) {
       navbar.classList.add('sticky');
     } else {
       navbar.classList.remove('sticky')
