b5a01e64f03480614d9c4e4775455ddff402e975
Michal Mirek <michal.mirek98@gmail.com>
2024-10-08 21:37:57 +0200

    [BC-9] validate form #2

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -1,8 +1,60 @@
+const CONTACT_FORM_SUBMIT_BUTTON_ID = 'contactFormSubmitButton';
+const NAME_INPUT_ID = 'name';
+const EMAIL_INPUT_ID = 'email';
+const EMAIL_MESSAGE_ID = 'msg';
+const ENABLED_SUBMIT_BTN_CLASS = 'btn-primary';
+const DISABLED_SUBMIT_BTN_CLASS = 'btn-secodnary';
+
 setTimeout(() => {
-  const submit = document.getElementById('contactFormSubmitButton');
+  const submit = document.getElementById(CONTACT_FORM_SUBMIT_BUTTON_ID);
   submit.addEventListener('click', () => sendEmail());
 }, 1000);
 
+
+function validate() {
+  const areFieldsCorrect = [validateName(), validateEmail(), validateMessage()]
+    .every(isValid => isValid);
+
+  if (areFieldsCorrect) {
+    enableSubmitButton();
+  } else {
+    disableSubmitButton();
+  }
+}
+
+function validateName() {
+  return !isEmpty(document.getElementById(NAME_INPUT_ID).value);
+}
+
+function validateEmail() {
+  const email = document.getElementById(EMAIL_INPUT_ID);
+  if (isEmpty(email.value)) {
+    return false;
+  }
+  return !email.validity.typeMismatch;
+}
+
+function validateMessage() {
+  return !isEmpty(document.getElementById(EMAIL_MESSAGE_ID).value);
+}
+
+function isEmpty(value) {
+  return !value;
+}
+
+function disableSubmitButton() {
+  const submit = document.getElementById(CONTACT_FORM_SUBMIT_BUTTON_ID);
+  submit.disabled = true;
+  submit.classList.remove(ENABLED_SUBMIT_BTN_CLASS);
+  submit.classList.add(DISABLED_SUBMIT_BTN_CLASS);
+}
+
+function enableSubmitButton() {
+  const submit = document.getElementById(CONTACT_FORM_SUBMIT_BUTTON_ID);
+  submit.disabled = false;
+  submit.classList.remove(DISABLED_SUBMIT_BTN_CLASS);
+  submit.classList.add(ENABLED_SUBMIT_BTN_CLASS);
+}
 
 
 function sendEmail() {
