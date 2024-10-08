fd1c9a5c6d5d6d2fdbfbfb2f561be845aa1c96bf
Michal Mirek <michal.mirek98@gmail.com>
2024-10-08 22:21:22 +0200

    [BC-9] configure contact form #2

--- src/js/contactForm.js
+++ src/js/contactForm.js
@@ -1,13 +1,18 @@
 const CONTACT_FORM_SUBMIT_BUTTON_ID = 'contactFormSubmitButton';
 const NAME_INPUT_ID = 'name';
 const EMAIL_INPUT_ID = 'email';
-const EMAIL_MESSAGE_ID = 'msg';
+const EMAIL_MESSAGE_INPUT_ID = 'msg';
+const PHONE_NUMBER_INPUT_ID = 'phone-number';
+const MESSAGE_TOPIC_INPUT_ID = 'msg-topic';
 const ENABLED_SUBMIT_BTN_CLASS = 'btn-primary';
 const DISABLED_SUBMIT_BTN_CLASS = 'btn-secodnary';
 
 setTimeout(() => {
   const submit = document.getElementById(CONTACT_FORM_SUBMIT_BUTTON_ID);
-  submit.addEventListener('click', () => sendEmail());
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    sendEmail();
+  });
 }, 1000);
 
 
@@ -35,7 +40,7 @@ function validateEmail() {
 }
 
 function validateMessage() {
-  return !isEmpty(document.getElementById(EMAIL_MESSAGE_ID).value);
+  return !isEmpty(document.getElementById(EMAIL_MESSAGE_INPUT_ID).value);
 }
 
 function isEmpty(value) {
@@ -58,17 +63,42 @@ function enableSubmitButton() {
 
 
 function sendEmail() {
-  console.log('trying to send email...');
+  const data = retrieveData();
+  console.log('trying to send email with data = ', data);
   Email.send({
     Host : "smtp.elasticemail.com",
     Username : "bc.smtp.proxy@gmail.com",
-    Password : "61C7791CE929D048D591CC6940BA0D78B755",
-    To : 'michal.mirek98@gmail.com',
-    From : "michal.mirek98@gmail.com",
-    Subject : "This is the subject",
-    Body : "And this is the body"
+    Password : "34539A0CC99F2026D59B46CC74BD47B9AB0D",
+    To : 'bc.smtp.proxy@gmail.com',
+    From : "bc.smtp.proxy@gmail.com",
+    Subject : `[Formularz kontaktowy] ${data.topic}`,
+    Body : `<div style="background-color: #292724; color: #FC0; padding: 20px; margin-bottom: 20px;">
+      <div><span>Email kontaktowy: ${data.email},</span></div>
+      <div><span>Imię i nazwisko: ${data.name},</span></div>
+      <div><span>Nr telefonu (+48): ${data.phone},</span></div>
+    </div>
+    <div>Treść wiadomości:</div>
+    <span style="font-style: italic;">${data.message}</span>
+    `
   }).then(
-    message => alert(message)
+    message => {
+      if (message === 'OK') {
+        console.log('Send email successfuly');
+        alert('Dziękujemy za kontakt. Skontaktujemy się z Tobą w przeciągu 24 godzin!')
+        return;
+      }
+      console.error('Failed to send email: ', message);
+      fbq('trackCustom', 'FailedToSendEmail', { error: message });
+    }
   );
 }
 
+function retrieveData() {
+  return {
+    name: document.getElementById(NAME_INPUT_ID).value,
+    email: document.getElementById(EMAIL_INPUT_ID).value,
+    phone: document.getElementById(PHONE_NUMBER_INPUT_ID).value,
+    topic: document.getElementById(MESSAGE_TOPIC_INPUT_ID).value,
+    message: document.getElementById(EMAIL_MESSAGE_INPUT_ID).value
+  }
+}
